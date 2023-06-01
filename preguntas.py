b18607d29a27f0b384b84c11cb349f7e2eda1fe8
manuel201807 <mamejiaf@unal.edu.co>
2023-06-01 11:04:29 -0500

    Update preguntas.py

--- preguntas.py
+++ preguntas.py
@@ -3,7 +3,7 @@ Regresión Lineal Univariada
 -----------------------------------------------------------------------------------------
 
 
-En este laboratorio se construirá un modelo de regresión lineal univariado.
+En este laboratio se construirá un modelo de regresión lineal univariado.
 
 
 """
